51cf74ff97ffb37aefd412dd7f0e2fd6bfce36a4
thebottedgoat <adam.k.momin@gmail.com>
2023-12-29 11:07:45 -0600

    kool

---
+++ src/Main.java
@@ -0,0 +1,112 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //---------------------------------------------------------------------------------------------------------------
+
+        //JFRAME
+
+        /*JFrame frame = new JFrame(); //creates frame
+
+
+        frame.setTitle("JAVA GUI"); //title
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //allows one to exit
+        frame.setSize(1920,1080); //sets res
+        //frame.setResizable(false); //can't change res
+        frame.setVisible(true); //makes seen
+        frame.getContentPane().setBackground(new Color(59, 114, 232));
+
+       frame.setLayout(null);
+
+
+
+
+       ImageIcon image = new ImageIcon("kool.jpg");
+        Border border = BorderFactory.createLineBorder(Color.GREEN,3);
+
+        //---------------------------------------------------------------------------------------------------------------
+
+        //JLABEL
+
+        JLabel label = new JLabel();
+        label.setText("Why am I so Cool?"); //set text of label
+        frame.add(label); // add label onto frame
+        label.setIcon(image); //add image
+
+        label.setHorizontalTextPosition(JLabel.CENTER); //alignment of text
+        label.setVerticalTextPosition(JLabel.TOP);
+
+
+        label.setForeground(new Color(227, 44, 44)); //color of text
+        label.setFont(new Font("Times New Roman",Font.BOLD,30)); //font
+        label.setIconTextGap(10); //gap from image
+
+        label.setBackground(Color.BLACK); //set backgroud color
+        label.setOpaque(true);
+
+        label.setBorder(border); //set border
+        label.setVerticalAlignment(JLabel.TOP); //alignment of img and text
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+       label.setBounds(100,100,800,800); //set x and y as well as dimensions
+
+        //frame.pack(); //add all the components, then pack. Fills entire thing
+
+
+        //---------------------------------------------------------------------------------------------------------------
+
+        //JPANEL
+
+        JPanel rPanel = new JPanel();
+        rPanel.setBackground(Color.red);
+        rPanel.setBounds(0,0,250,250);
+        //rPanel.setLayout(new BorderLayout()); //this allows you to move around the label
+        rPanel.setLayout(null);
+
+
+        JPanel bPanel = new JPanel();
+        bPanel.setBackground(Color.blue);
+        bPanel.setBounds(250,0,250,250);
+
+        JPanel gPanel = new JPanel();
+        gPanel.setBackground(Color.green);
+        gPanel.setBounds(0,250,500,250);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(750,750);
+        frame.setVisible(true);
+
+        JLabel label = new JLabel();
+        label.setText("HELLO!");
+        label.setForeground(Color.BLACK);
+        //label.setHorizontalAlignment(JLabel.CENTER);
+        //label.setVerticalAlignment(JLabel.CENTER);
+        label.setBounds(0,0,75,75);
+
+        rPanel.add(label);
+
+        frame.add(rPanel);
+        frame.add(bPanel);
+        frame.add(gPanel);
+
+        //NOTE: When .setLayout is made null, you need to use .setBounds
+
+
+        //---------------------------------------------------------------------------------------------------------------
+
+        //BUTTONS
+
+        new MyFrame();
+
+         */
+
+        //---------------------------------------------------------------------------------------------------------------
+
+
+    }
+    }
---
+++ src/MyFrame.java
@@ -0,0 +1,46 @@
+/*import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends Main implements ActionListener {
+
+
+//BUTTONS
+    JButton button;
+    MyFrame() {
+        //BUTTONS
+
+        ImageIcon image = new ImageIcon("thumb.jpg");
+
+        button = new JButton();
+        button.setBounds(0, 0, 500, 500);
+        button.addActionListener(e -> System.out.println("cool"));
+        button.setText("I'm A Button");
+        button.setFocusable(false);
+        button.setIcon(image);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setFont(new Font("Times New Roman", Font.BOLD, 30));
+        button.setIconTextGap(50);
+        button.setBorder(BorderFactory.createEtchedBorder());
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.add(button);
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
+
+ */
+
